b84652056a1e9060f110ce6836f4030f8bce3123
skochko156 <70753325+skochko156@users.noreply.github.com>
2020-12-16 20:57:14 +0200

    Add files via upload

---
+++ ls/Armor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace oooop
+{
+    public class Armor : Item, IWearable
+ 	{
+		 private int damage_coeff;
+		 
+ 		public Weapon(string name, int damage_coeff)
+ 			:base(name)
+ 		{
+			 this.damage_coeff = damage_coeff;
+ 		}
+
+ 		public void Wear(Player p)
+ 		{
+			 
+ 		}
+ }
---
+++ ls/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace oooop
+{
+    public class Weapon : Item, IWearable
+ 	{
+		 private int damage;
+
+ 		public Weapon(string name, int damage)
+ 			:base(name)
+ 		{
+			 this.damage = damage;
+ 		}
+
+ 		public void Wear(Player p)
+ 		{
+			 
+ 		}
+ }
+
